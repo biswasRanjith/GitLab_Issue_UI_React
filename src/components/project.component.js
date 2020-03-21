12549e9a34286a5cfe07f509bc9b34f54018360e
ranjithbiswas755 <ranjith_biswas@thbs.com>
2020-03-21 20:08:54 +0530

    side nav added

--- src/components/project.component.js
+++ src/components/project.component.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ReactDOM from "react-dom";
 import {Link} from 'react-router-dom';
-import './navbar.css';
+import './project.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default class project extends Component {
@@ -23,6 +23,95 @@ export default class project extends Component {
                             </div>
                         </a>
                     </div>
+                <ul className="sidebar_toplevel_item">
+                    <li className="sidebar_li">
+                        <a className="sidebar_a" href="#">
+
+<span className="nav_item_names">
+    Project Overview
+</span>
+                        </a>
+                        <ul className="sidebar_sublevel_items">
+                            {/* <li className="flyout_items">
+                                <a className="flyout_items_a" href="#">
+                                    <strong className="flyout_items_strong">
+                                        Project Overview
+                                    </strong>
+                                </a>
+                            </li> */}
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">Details</span>
+    </a>
+</li>
+
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">Activity</span>
+    </a>
+</li>
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">Release</span>
+    </a>
+</li>
+       
+                        </ul>
+                    </li>
+<li className="sidebar_li">
+                        <a className="sidebar_a" href="#">
+
+<span className="nav_item_names">
+    Issues
+</span>
+                        </a>
+                        <ul className="sidebar_sublevel_items">
+                            {/* <li className="flyout_items">
+                                <a className="flyout_items_a" href="#">
+                                    <strong className="flyout_items_strong">
+                                        Project Overview
+                                    </strong>
+                                </a>
+                            </li> */}
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">List</span>
+    </a>
+</li>
+
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">Boards</span>
+    </a>
+</li>
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">Labels</span>
+    </a>
+</li>
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">Service Desk</span>
+    </a>
+</li>
+<li className="flyout_divider"></li>
+<li className="sublevel_inner">
+    <a className="sublevel_a" href="#">
+        <span className="sublevel_span">Milestones</span>
+    </a>
+</li>
+                        </ul>
+                    </li>
+<li>bkjbkljn</li>
+                </ul>
+
                 </div>
             </div>
         </div>
